0cde29bd677423056a9940ae693db0c296f7b4fb
barcofourie <barco.fourie@gmail.com>
2023-05-12 15:44:39 +0200

    Update logger_file_backend.ex

--- lib/logger_file_backend.ex
+++ lib/logger_file_backend.ex
@@ -146,7 +146,25 @@ defmodule LoggerFileBackend do
   end
 
   defp format_event(level, msg, ts, md, %{format: format, metadata: keys}) do
-    Logger.Formatter.format(format, level, msg, ts, take_metadata(md, keys))
+    format(level, msg, ts, md, keys)
+  end
+  
+  import Jason.Helpers, only: [json_map: 1]
+
+  def format(level, msg, ts, md, md_keys) do
+    json_map(
+      time: FormatterUtils.format_timestamp(ts),
+      severity: Atom.to_string(level),
+      message: IO.chardata_to_string(msg),
+      metadata: format_metadata(md, md_keys)
+    )
+  end
+
+  defp format_metadata(md, md_keys) do
+    md
+    |> LoggerJSON.take_metadata(md_keys, @processed_metadata_keys)
+    |> FormatterUtils.maybe_put(:error, FormatterUtils.format_process_crash(md))
+    |> JasonSafeFormatter.format()
   end
 
   @doc false
